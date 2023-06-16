34403d31bf8d8ac556d9a1848e3ba166631a000a
Tristenh <tristenhobba1@gmail.com>
2023-06-16 15:45:34 +1000

    added JSONParse function, inputJSON getItem(inputValue)mif inputJSON the array gets JSON.parse(inputJSON)

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const form = document.querySelector(".form");
 const input = document.querySelector(".input");
 const submit = document.querySelector(".submit");
 const results = document.querySelector(".results");
+
+let inputArray = [];
+
+function JSONParse() {
+  let inputJSON = localStorage.getItem("inputValue");
+  if (inputJSON) {
+    inputArray = JSON.parse(inputJSON);
+  }
+}

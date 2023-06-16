cd0bc36b1c8a7528ada142d329220c20e691c6b4
Tristenh <tristenhobba1@gmail.com>
2023-06-16 15:19:12 +1000

    added html form with input and submit buttons with results div, add query selectors

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const submit = document.querySelector(".submit");
+const results = document.querySelector(".results");
